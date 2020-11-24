f4064b7e7db09e54434fda1e8237cbdcae49b4f8
Andrew Koehn <akoehn@vt.edu>
2020-11-24 12:20:19 -0500

    Implemented usage error handling

--- manip.py
+++ manip.py
@@ -1,3 +1,12 @@
 #Andrew Koehn
 #11/23/20
 #Project 3
+import sys
+
+if len(sys.argv) == 1:
+	print("Usage: python manip.py <OPERATION> [PARAMETERS] <FILE>:")
+	sys.exit(1)
+
+if sys.argv[1] != "-a" and sys.argv[1] != "-r" and sys.argv[1] != "-f" and sys.argv[1] != "-c" and sys.argv[1] != "-t":
+	print("Operation not recognized. Try -a, -r, -f, -c, or -t")
+	sys.exit(2)
