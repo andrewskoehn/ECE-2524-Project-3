03b3b9b9233c49a8f8d008b7ced9c077c7490b51
Andrew Koehn <akoehn@vt.edu>
2020-11-24 12:32:56 -0500

    Implemented file error handling

--- manip.py
+++ manip.py
@@ -10,3 +10,12 @@ if len(sys.argv) == 1:
 if sys.argv[1] != "-a" and sys.argv[1] != "-r" and sys.argv[1] != "-f" and sys.argv[1] != "-c" and sys.argv[1] != "-t":
 	print("Operation not recognized. Try -a, -r, -f, -c, or -t")
 	sys.exit(2)
+	
+try:
+	file = open(sys.argv[len(sys.argv)-1], 'r')
+except OSError:
+	print("File does not exist or cannot be opened.")
+	sys.exit(3)
+
+
+file.close()
