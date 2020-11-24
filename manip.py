076e1c7658982995a3b6d64dd489fb6daacb699a
Andrew Koehn <akoehn@vt.edu>
2020-11-24 15:22:07 -0500

    Added comments

--- manip.py
+++ manip.py
@@ -1,41 +1,40 @@
-#Andrew Koehn
-#11/23/20
-#Project 3
+#################
+# Andrew Koehn
+# 11/24/20
+# Project 3
+
+# Modules
 import sys
 import re
 
-
+# Usage error handling
 if len(sys.argv) < 4:
-	print("Usage: python manip.py <OPERATION> [PARAMETERS] <FILE>")
+	print("Usage: python manip.py <OPERATION> <PARAMETERS> <FILE>")
 	sys.exit(1)
 
-if sys.argv[1] != "-a" and sys.argv[1] != "-r" and sys.argv[1] != "-f" and sys.argv[1] != "-c" and sys.argv[1] != "-t":
-	print("Operation not recognized. Try -a, -r, -f, -c, or -t")
-	sys.exit(2)
-
+# File error handling
 test = re.search("\.", sys.argv[len(sys.argv)-1])
 if test == None:
-	print("Usage: python manip.py <OPERATION> [PARAMETERS] <FILE>")
+	print("Usage: python manip.py <OPERATION> <PARAMETERS> <FILE>")
 	sys.exit(1)
-
 try:
 	file = open(sys.argv[len(sys.argv)-1], 'r')
 except OSError:
 	print("File does not exist or cannot be opened.")
 	sys.exit(3)
 
-
-
+# Append (-a) implementation
 if sys.argv[1] == "-a":
+	# Usage error handling
 	if len(sys.argv) != 4:
 		print("Usage: python manip.py -a <STRING> <FILE>")
 		sys.exit(1)
 	newText = file.read()
 	newText += sys.argv[2]
 
-
-
+# Remove (-r) implementation
 elif sys.argv[1] == "-r":
+	# Usage error handling
 	if len(sys.argv) != 4 or not sys.argv[2].isnumeric():
 		print("Usage: python manip.py -r <LINE NUMBER> <FILE>")
 		sys.exit(1)
@@ -46,6 +45,7 @@ elif sys.argv[1] == "-r":
 	newText = ""
 	lineNumber = 1
 	found = False
+	# Read through file and skip appropriate line
 	for line in file:
 		if lineNumber == num:
 			found = True
@@ -53,13 +53,14 @@ elif sys.argv[1] == "-r":
 		else:
 			lineNumber += 1
 			newText += line
+	# Range error handling
 	if not found:
 		print("File does not contain that many lines.")
 		sys.exit(2)
 
-
-
+# Find/replace (-f) implementation
 elif sys.argv[1] == "-f":
+	# Usage error handling
 	if len(sys.argv) != 5:
 		print("Usage: python manip.py -f <FIND> <REPLACE> <FILE>")
 		sys.exit(1)
@@ -67,29 +68,34 @@ elif sys.argv[1] == "-f":
 	replace = sys.argv[3]
 	newText = file.read()
 	oldText = newText
+	# Use str.replace built-in function
 	newText = newText.replace(find, replace)
+	# No occurrence handling
 	if newText == oldText:
-		print("No occurences of \""+find+"\" were found.")
+		print("No occurrences of \""+find+"\" to be replaced.")
 		sys.exit(2)
 
-
-
+# Count (-c) implementation
 elif sys.argv[1] == "-c":
+	# Usage error handling
 	if len(sys.argv) != 4:
 		print("Usage: python manip.py -c <STRING> <FILE>")
 		sys.exit(1)
 	string = sys.argv[2]
-	newText = file.read()
-	numOccurences = newText.count(string)
-	if numOccurences == 0:
-		print("No occurences of \""+string+"\" found.")
+	text = file.read()
+	# Use str.count built-in function
+	numOccurrences = text.count(string)
+	# No occurrence handling
+	if numOccurrences == 0:
+		print("No occurrences of \""+string+"\" found.")
 		sys.exit(2)
 	else:
-		print(numOccurences,"occurence(s) of \""+string+"\" found.")
+		print(numOccurrences,"occurrence(s) of \""+string+"\" found.")
+		sys.exit(0)
 
-
-
+# Comment (-t) implementation
 elif sys.argv[1] == "-t":
+	# Usage error handling
 	if len(sys.argv) != 6 or not sys.argv[3].isnumeric() or not sys.argv[4].isnumeric():
 		print("Usage: python manip.py -t <COMMENT> <START> <END> <FILE>")
 		sys.exit(1)
@@ -102,6 +108,7 @@ elif sys.argv[1] == "-t":
 	newText = ""
 	lineNumber = 1
 	found = False
+	# Read through file and add comment character at beginning of appropriate lines
 	for line in file:
 		if lineNumber >= start and lineNumber <= end:
 			found = True
@@ -110,16 +117,19 @@ elif sys.argv[1] == "-t":
 		else:
 			newText += line
 			lineNumber += 1
+	# Range error handling
 	if not found or lineNumber < end+1:
 		print("File does not contain the given line range.")
 		sys.exit(2)
-	
-	
 
-
-
+# Usage error handling
+else:
+	print("Operation not recognized. Try -a, -r, -f, -c, or -t")
+	sys.exit(2)
 
 file.close()
+
+# Update file with changes
 file = open(sys.argv[len(sys.argv)-1], 'w')
 file.write(newText)
 file.close()
