cd40be6aff4a2b4b3ddd2a0abf3d3aa9a642d16d
Andrew Koehn <akoehn@vt.edu>
2020-11-24 12:42:30 -0500

    Implemented -a option

--- manip.py
+++ manip.py
@@ -4,7 +4,7 @@
 import sys
 
 if len(sys.argv) == 1:
-	print("Usage: python manip.py <OPERATION> [PARAMETERS] <FILE>:")
+	print("Usage: python manip.py <OPERATION> [PARAMETERS] <FILE>")
 	sys.exit(1)
 
 if sys.argv[1] != "-a" and sys.argv[1] != "-r" and sys.argv[1] != "-f" and sys.argv[1] != "-c" and sys.argv[1] != "-t":
@@ -17,5 +17,28 @@ except OSError:
 	print("File does not exist or cannot be opened.")
 	sys.exit(3)
 
+if sys.argv[1] == "-a":
+	if len(sys.argv) != 4:
+		print("Usage: python manip.py -a <STRING> <FILE>")
+		sys.exit(1)
 
+	newText = file.read()
+	newText += sys.argv[2]
+
+'''
+elif sys.argv[1] == "-r":
+	if len(sys.argv) != 4:
+		print("Usage: python manip.py -r <LINE NUMBER> <FILE>")
+		sys.exit(1)
+	elif sys.argv[
+	lineNumber = 1
+'''
+	
+
+
+
+
+file.close()
+file = open(sys.argv[len(sys.argv)-1], 'w')
+file.write(newText)
 file.close()
